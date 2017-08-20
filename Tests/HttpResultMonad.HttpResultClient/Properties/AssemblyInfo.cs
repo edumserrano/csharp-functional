b2aab8bfce612b2d0cac56c783b12a7ec6cd1dff
Eduardo Miguel Serrano <eduardomserrano@outlook.com>
2017-08-20 02:20:59 +0100

    Updated and consolidated nuget packages

---
+++ Tests/HttpResultMonad.HttpResultClient/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("HttpResultMonad.HttpResultClient")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HttpResultMonad.HttpResultClient")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("852c5a63-381c-4078-a76c-1c6925b1ed94")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tests/HttpResultMonad.HttpResultClient/UnitTest1.cs
@@ -0,0 +1,12 @@
+﻿using Xunit;
+
+namespace HttpResultMonad.HttpResultClient
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+        }
+    }
+}
