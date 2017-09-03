b2a91b98e454860c16676b036cd3cb6888506c1f
Eduardo Miguel Serrano <eduardomserrano@outlook.com>
2017-09-03 16:04:49 +0100

    Re-added HttpResultMonad.HttpResultOnHttpClient Tests

---
+++ Tests/HttpResultMonad.HttpResultOnHttpClient.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("HttpResultMonad.HttpResultOnHttpClient.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HttpResultMonad.HttpResultOnHttpClient.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("247829f4-d0dc-42df-a916-e1559360f401")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tests/HttpResultMonad.HttpResultOnHttpClient.Tests/TestHttpClientHandler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HttpResultMonad.HttpResultOnHttpClient.Tests
+{
+    internal class TestHttpClientHandler : HttpClientHandler
+    {
+        private readonly Func<Task<HttpResponseMessage>> _func;
+
+        public TestHttpClientHandler(Func<Task<HttpResponseMessage>> func)
+        {
+            _func = func;
+        }
+
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            return _func();
+        }
+    }
+}
