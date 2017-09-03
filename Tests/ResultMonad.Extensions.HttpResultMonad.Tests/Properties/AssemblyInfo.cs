3110c5c38a09af05fca40a0c8a14fc948446a5af
Eduardo Miguel Serrano <eduardomserrano@outlook.com>
2017-09-03 18:07:59 +0100

    Re-added ResultMonad.Extensions.HttpResultMonad tests

---
+++ Tests/ResultMonad.Extensions.HttpResultMonad.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ResultMonad.Extensions.HttpResultMonad.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ResultMonad.Extensions.HttpResultMonad.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d5f8af9c-2233-4c4c-9832-32b8e718f6a4")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
