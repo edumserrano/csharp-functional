3a0a31b00c27edd34dd597efd30d310d411c2ece
Eduardo Miguel Serrano <eduardomserrano@outlook.com>
2017-09-03 17:01:51 +0100

    Re-added MaybeMonad.Extensions.ResultMonad tests

---
+++ Tests/MaybeMonad.Extensions.ResultMonad.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MaybeMonad.Extensions.ResultMonad.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MaybeMonad.Extensions.ResultMonad.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("452531a0-dad8-4ec1-9da5-bdbc327ae983")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
